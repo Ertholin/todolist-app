28c11be4536fa61b49921dd894b4026655a28c46
Ertholin <67572982+Ertholin@users.noreply.github.com>
2023-04-11 02:56:42 -0400

    Refactoring with creating a separate date.js to show the date by exporting our results of one of our functions and using it in our app.js

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const date = require(__dirname + "/date.js")
 
 const app = express();
-let items = ["Buy food", "Cook food", "Eat Food"];
-let workItems = [];
+const items = ["Buy food", "Cook food", "Eat Food"];
+const workItems = [];
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"))
 
 app.get("/", function(req, res){
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
 
-  let day = today.toLocaleDateString("en-US", options);
+  let day = date.getDate();
+
   res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res){
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   console.log(req.body);
   if(req.body.list === "Work"){
     workItems.push(item);
@@ -39,14 +35,8 @@ app.get("/work", function(req, res){
 
 app.get("/about", function(req, res){
   res.render("about");
-})
-
-// app.post("/work", function(req, res){
-//   let item = req.body.newItem;
-//   workItems.push(item);
-//   res.redirect("/work");
-// });
+});
 
 app.listen(3000, function(){
   console.log("Server started on port 3000")
-})
+});
